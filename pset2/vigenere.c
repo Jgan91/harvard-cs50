8826f7704644d05187350acf4526106463d4719e
Justin Gan <>
2017-08-01 02:38:19 +0000

    Refactored Caesar Cipher

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -5,30 +5,23 @@
 
 int main(int argc, string argv[])
 {
-    // print error if user doesn't input argv
     if (argc != 2)
     {
         printf("missing command-line argument\n");
         return 1;
     }
     
-    // store input as k 
     int k = atoi(argv[1]);
-    
-    // ask user for plaintext input
     printf("plaintext: ");
     string p = get_string();
     
     printf("ciphertext: ");
-    
-    // iterate over characters in input
     for (int i = 0; i < strlen(p); i++)
     {
         if (isalpha(p[i]))
         {
             if (isupper(p[i]))
             {
-                // print rotated letter by k positions 
                 printf("%c", ((p[i] - 65 + k) % 26 + 65));
             } else if (islower(p[i]))
             {
