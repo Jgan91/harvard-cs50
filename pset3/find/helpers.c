b0a1dd4ec9844d67ba33be47f0f9f1d41481bf68
Justin Gan <>
2017-08-02 05:49:46 +0000

    Completed find.c

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -22,6 +22,8 @@ bool search(int value, int values[], int n)
         return false;
     }
     
+    // linear search
+    /*
     for (int i = 0; i < n; i++)
     {
         if (values[i] == value)
@@ -29,34 +31,19 @@ bool search(int value, int values[], int n)
             return true;
         }
     }
+    */
     
+    
+    if (binsearch(value, values, 0, n))
+    {
+        return true;
+    }
+
+    // Return false if value is not in values.
+
     return false;
     
-    //binsearch(value, values, 0, n);
-    /*
-    int start = 0;
-    int end = n - 1;
-    // Return true if value is in values
-    // Find middle of values
     
-    int middle = n / 2;
-        // Return true if middle == value
-        if (values[middle] == value)
-        {
-            return true;
-        // Check if value is smaller or larger than middle
-        } else if (value < values[middle])
-        // Search appropriate half
-        {
-            search(value, values)
-        } else if (value > values[middle])
-        // Search appropriate half
-        {
-            start = middle + 1;
-            search(value, values, middle)
-        }
-    */
-    // Return false if value is not in values.
     
 }
 
@@ -65,14 +52,13 @@ bool search(int value, int values[], int n)
  */
 void sort(int values[], int n)
 {
-    // TODO: implement a sorting algorithm
     // set swap counter to non-zero value
     int swap = -1;
     // repeat until swap counter is zero:
     while (swap != 0)
     {
         // reset swap counter to 0
-        swap = 0
+        swap = 0;
         // look at each adjacent pair
         for (int i = 0; i < n - 1; i++)
         {
@@ -86,39 +72,47 @@ void sort(int values[], int n)
             }
         }
     }
-        
-    
-        
-    
+
     return;
 }
 
 
-/*
+
 bool binsearch(int value, int values[], int start, int end)
 {
     // Calculate middle
-    int mid = (start + end) / 2;
+    int mid;
+    /* if (start == end)
+    {
+     mid = 0;
+    } else
+    {
+    */
+    if (start > end) {
+        return false;
+    }
+        mid = (start + end) / 2;
+    // }
     // Return true if middle == value
-    if (values[mid] == value)
+    if (start == end && values[mid] != value)
+    {
+        return false;
+    } else if (values[mid] == value)
     {
         return true;
     }
     
     // Change end to left of middle if value < middle
     if (value < values[mid]) {
-        start = mid - 1;
+        end = mid - 1;
         // Binary Search
-        binsearch(value, values, start, end);
+        return binsearch(value, values, start, end);
+    } // Change start to right of middle if value > middle
+    else if (value > values[mid]) {
+        start = mid + 1;
+        // Binary Search
+        return binsearch(value, values, start, end);
     }
-        
-        
-    // Change start to right of middle if value > middle
-        // Binary Search
-    if (value > values[mid]) {
-        end = mid + 1;
-        // Binary Search
-        binsearch(value, values, start, end);
-    }
+    
+    return false;
 }
-*/
